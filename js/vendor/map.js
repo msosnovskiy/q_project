5e20afd16fa8a2f2a8c2a185824f33e9a435254b
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2021-06-18 10:18:52 +0300

    Updates

--- js/vendor/map.js
+++ js/vendor/map.js
@@ -22,6 +22,10 @@ function init() {
   myMap.controls.remove('searchControl');
   myMap.controls.remove('trafficControl');
   //отключаем зум колёсиком мышки
-  // myMap.behaviors.disable('scrollZoom');
-  myMap.behaviors.disable('drag');
+  myMap.behaviors.disable('scrollZoom');
+  //на мобильных устройствах... (проверяем по userAgent браузера)
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    //... отключаем перетаскивание карты
+    myMap.behaviors.disable('drag');
+  }
 }
