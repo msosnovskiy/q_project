110588b9833d83b3aaa200f0bb2c4be6ddb7f3ee
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2021-06-21 14:18:40 +0300

    edit map js

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -5,7 +5,7 @@ export default class Map {
   }
 
   setEventListener() {
-    this.map.addEventListener('touchmove', handler, {passive: true}, (event) => {
+    this.map.addEventListener('touchmove', (event) => {
       if (this.map.contains(event.target) && (event.targetTouches.length < 2)) {
         this.map.classList.add(this.classPrompt);
       }
