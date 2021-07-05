f779b1218a1819ef6f24bd0d575f0e0af060a2a5
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2021-07-06 01:58:36 +0300

    small fix

--- src/js/components/NewsPreviewList.js
+++ src/js/components/NewsPreviewList.js
@@ -14,34 +14,24 @@ export default class NewsPreviewList {
     else return 2021;
   }
 
-  _sortArrayByDays(array) {
+  _sortArrayByDate(array) {
     return array.data.sort((a, b) => a.date.split('.')[2] + a.date.split('.')[1] + a.date.split('.')[0] < b.date.split('.')[2] + b.date.split('.')[1] + b.date.split('.')[0] ? 1 : -1);
   }
-
-  _addItem(itemElement) {
-    this.container.appendChild(itemElement);
-  }
-
-  // _findMaxYear(array) {
-  //   return array.reduce((acc, curr) => acc.year > curr.year ? acc : curr);
-  // }
-
-  // _getItemForRender(array, count) {
-  //   for (let i = 0; i < count; i++) {
-  //     // return this._sortArrayByDays(array)[i];
-  //   }
-  // }
-
+  
   _findArrayByYear = (array, value) => array.filter((item) => {
     return item.year == value;
   })[0]
+  
+  _addItem(itemElement) {
+    this.container.appendChild(itemElement);
+  }
 
   render() {
     this.container.innerHTML = '';
     let stepYear = 0;
 
     while (this.newsPreviewLength > 0) {
-      let newsPreviewArray = this._sortArrayByDays(this._findArrayByYear(this.array, this._gettingСurrentYear() - stepYear));
+      let newsPreviewArray = this._sortArrayByDate(this._findArrayByYear(this.array, this._gettingСurrentYear() - stepYear));
       stepYear++;
       newsPreviewArray.forEach((item) => {
         if (this.newsPreviewLength > 0) {
